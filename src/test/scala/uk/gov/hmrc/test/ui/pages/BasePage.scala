df6de29dc63287d371081ceee9cb3788a4178d2f
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2023-10-09 15:49:48 +0100

    PLATUI-2539 - Migrating team repo to test ui-test-runner and sbt-test-report

--- src/test/scala/uk/gov/hmrc/test/ui/pages/BasePage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/BasePage.scala
@@ -1,8 +1,84 @@
 package uk.gov.hmrc.test.ui.pages
 
-import org.openqa.selenium.By
-import org.scalatest.matchers.should.Matchers
-import uk.gov.hmrc.test.ui.driver.BrowserDriver
+import org.openqa.selenium.support.ui.{ExpectedConditions, FluentWait}
+import org.openqa.selenium.{By, WebDriver}
+import org.scalatest.matchers.must.Matchers
+import org.scalatestplus.selenium.Page
+
+import java.time.Duration
+import java.time.temporal.ChronoUnit
+import scala.util.Random
+
+object BasePage extends BasePage {
+  val url = ""
+  val title = "No title provided"
+}
+
+trait BasePage extends Matchers with Page {
+  val driver = Driver.instance
+  val UNKNOWN = "Unknown property"
+
+  val url: String
+  val title: String
+
+  def navTo() = navigateTo(s"$basePageUrl/$url")
+
+  lazy val basePageUrl: String = {
+    val environmentProperty = System.getProperty("environment", "local").toLowerCase
+    environmentProperty match {
+      case "local" => "http://localhost:9970/register-your-company"
+      case "qa" => "https://www.qa.tax.service.gov.uk/register-your-company"
+      case _ => throw new IllegalArgumentException(s"Environment '$environmentProperty' not known")
+    }
+  }
+
+  lazy val registerYourBusinessBasePageUrl: String = {
+    val environmentProperty = System.getProperty("environment", "local").toLowerCase
+    environmentProperty match {
+      case "local" => "http://localhost:9634/register-your-business"
+      case "qa" => "https://www.qa.tax.service.gov.uk/register-your-business"
+      case _ => throw new IllegalArgumentException(s"Environment '$environmentProperty' not known")
+    }
+  }
+
+  lazy val iiBaseUrl: String = {
+    val environmentProperty = System.getProperty("environment", "local").toLowerCase
+    environmentProperty match {
+      case "local" => "http://localhost:9976/incorporation-information"
+      case "qa" => "https://www.qa.tax.service.gov.uk/incorporation-information"
+      case _ => throw new IllegalArgumentException(s"Environment '$environmentProperty' not known")
+    }
+  }
+
+
+  def waitForPageToBeLoaded(condition: => Boolean, exceptionMessage: String, timeoutInSeconds: Int = 30) : Unit = {
+    val endTime = System.currentTimeMillis + timeoutInSeconds * 1000
+    while (System.currentTimeMillis < endTime) {
+      try {
+        if (condition) return
+      } catch {
+        case _: RuntimeException => ()
+      }
+      Thread.sleep(500)
+    }
+    throw new Exception(exceptionMessage + "\n<PAGE LOAD TIMEOUT> The current page was: " + driver.getCurrentUrl + " with title " + driver.getTitle)
+  }
+
+  def secondsWait(secs: Int) = Thread.sleep(secs.*(1000))
+
+  def deletecookies() = driver.manage().deleteAllCookies()
+
+  private val randomCred = (new Random()).nextInt(1000000000)
+
+  // Page Helpers
+
+  def incorpId: Boolean = findById("incorpID").isDisplayed
+
+  def navigateTo(url: String) = driver.navigate().to(url)
+
+  def findById(id: String) = driver.findElement(By.id(id))
+
+  def findByCSS(cssClass: String) = driver.findElement(By.cssSelector(cssClass))
 
   def findAllByCSS(htmlClass: String) = driver.findElements(By.cssSelector(htmlClass))
 
